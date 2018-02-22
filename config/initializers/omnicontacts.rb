0b34e1515df006e920e97d617427caaeddfde698
Sandip Bhowmick <bhowmick.sandip@gmail.com>
2018-02-22 21:29:30 +0530

    Update omnicontacts.rb

--- config/initializers/omnicontacts.rb
+++ config/initializers/omnicontacts.rb
@@ -1,5 +1,5 @@
 require "omnicontacts"
 
 Rails.application.middleware.use OmniContacts::Builder do
-  importer :gmail, "179124996790-d513c0dvfh6lo3s6945eotrq8fuqen4f.apps.googleusercontent.com", "6XKpUpUCy0otyScBz-KAA0z4", {:redirect_path => "/contacts/gmail/contact_callback"}
+  importer :gmail, "", "", {:redirect_path => "/contacts/gmail/contact_callback"}
 end
